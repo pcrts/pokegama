c23e7990888dcc71a7f4d25d1900ca32660dc9fe
Paulo <paulo.cortes@casaevideo.com.br>
2020-07-08 22:21:17 -0300

    Pegar dados da API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import logo from "./img/logo.png";
 import buscar from "./img/buscar.png";
 import bulba from "./img/bulba.png";
@@ -6,251 +7,87 @@ import pokeball from "./img/pokeball.png";
 import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [cart, setCart] = useState([]);
+  function getPokemon() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then(res => {
+      res.data.results.map(item => {
+        axios.get(item.url).then(({ data }) => {
+          setPokemon(pokemon => [
+            ...pokemon,
+            { ...data, price: (Math.random() * 1000 + 1).toFixed(2) }
+          ]);
+          console.log(data);
+        });
+      });
+    });
+  }
+  useEffect(() => {
+    (async () => {
+      await getPokemon();
+    })();
+  }, []);
+  useEffect(() => console.log(cart), [cart]);
   return (
     <div className="App">
-      <div className="header" id="header"> 
+      <div className="header" id="header">
         <div>
           <img src={logo} />
         </div>
         <div className="search">
-          <input id="busca" className="busca" placeholder="Busque aqui!" /> 
+          <input id="busca" className="busca" placeholder="Busque aqui!" />
           <button id="buscar" className="btn-buscar">
             <img src={buscar} />
           </button>
         </div>
-        <div className="spacer">
-        </div>
+        <div className="spacer"></div>
       </div>
 
-      <div className="grid"> 
-        <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-     <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
-
-           <div className="card">
-          <div>
-            <img src={bulba} />
-          </div>
-          <div className="infos">
-            <h5>Bulbasaur</h5>
-            <div className="tipos">  
-              <p className="grass">Grass</p>
-              <p className="poison">Poison</p>
-            </div>
-            <h6 className="preco">R$ 10.00</h6>
-          </div>
-          <button className="adicionar">Eu quero!</button>
-        </div>
+      <div className="grid">
+        {pokemon.length
+          ? pokemon.map(item => (
+              <div className="card">
+                <div>
+                  <img src={item.sprites.front_default} />
+                </div>
+                <div className="infos">
+                  <h5>{item.name}</h5>
+                  <div className="tipos">
+                    {item.types.map(({ type }) => (
+                      <p className={type.name}>{type.name}</p>
+                    ))}
+                  </div>
+                  <h6 className="preco">R$ {item.price}</h6>
+                </div>
+                <button
+                  className="adicionar"
+                  onClick={() => setCart(carrinho => [...carrinho, item])}
+                >
+                  Eu quero!
+                </button>
+              </div>
+            ))
+          : null}
       </div>
-  
-  <div className="carrinho">
-  <div className="cart-header">
-    <img src={pokeball} />
-    <h3>Meu Carrinho</h3>
-  </div>
 
-  <div className="card-holder">
-    <div className="cart-card">
-      <img src={bulba} />
-      <h5>Bulbasaur</h5>
-      <h6 className="preco">R$ 4.00</h6>
+      <div className="carrinho">
+        <div className="cart-header">
+          <img src={pokeball} />
+          <h3>Meu Carrinho</h3>
+        </div>
+
+        <div className="card-holder">
+          {cart.map(item => (
+            <div className="cart-card">
+              <img src={item.sprites.front_default} />
+              <h5>{item.name}</h5>
+              <h6 className="preco">{item.price}</h6>
+            </div>
+          ))}
+        </div>
+        <button className="finalizar">Finalizar compra</button>
+      </div>
     </div>
-
-
-  <div className="cart-card">
-      <img src={bulba} />
-      <h5>Bulbasaur</h5>
-      <h6 className="preco">R$ 4.00</h6>
-    </div>
-
-  <div className="cart-card">
-      <img src={bulba} />
-      <h5>Bulbasaur</h5>
-      <h6 className="preco">R$ 4.00</h6>
-    </div>
-  </div>
-  
-  <button className="finalizar">Finalizar compra</button>
-</div>
-
-    
-    </div> 
   );
 }
 
